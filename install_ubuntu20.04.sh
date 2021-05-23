a6d89fca07c87424ed1e9682efee31af3359965b
petermeng_01 <petermeng_01@163.com>
2021-05-22 21:51:34 -0700

    add bat install

--- install_ubuntu20.04.sh
+++ install_ubuntu20.04.sh
@@ -28,7 +28,7 @@ fi
 cd ~/Documents/vim
 git pull
 sudo apt-get update
-sudo apt-get install -y libncurses5-dev libgtk2.0-dev libatk1.0-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev python3-dev ruby-dev lua5.1 liblua5.1-0-dev libperl-dev git dos2unix sed cscope id-utils exuberant-ctags fontconfig language-pack-zh-hans libncurses5-dev exuberant-ctags silversearcher-ag gawk libncurses5 libtinfo5
+sudo apt-get install -y libncurses5-dev libgtk2.0-dev libatk1.0-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev python3-dev ruby-dev lua5.1 liblua5.1-0-dev libperl-dev git dos2unix sed cscope id-utils exuberant-ctags fontconfig language-pack-zh-hans libncurses5-dev exuberant-ctags silversearcher-ag gawk libncurses5 libtinfo5 ripgrep bat
 make distclean
 pythonName=`find /usr/lib/python3.* -maxdepth 0 -type d`
 if [[ ${pythonName} ]]; then
